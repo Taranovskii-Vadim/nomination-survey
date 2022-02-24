ca482f20d03ef0c7348f2b222d12c8375c22f442
vgTaranovsky <taranovscki.vadim@yandex.ru>
2022-02-24 23:05:00 +0400

    feat(master): chart refactor

--- front/src/components/BarChart/index.tsx
+++ front/src/components/BarChart/index.tsx
@@ -3,7 +3,6 @@ import { Bar } from "react-chartjs-2";
 import { Flex, Text } from "@chakra-ui/react";
 
 import { ChartData } from "../../store/surveyStore/types";
-import { getColors } from "./helpers";
 import { FireIcon } from "../icons";
 
 interface Props {
@@ -24,8 +23,6 @@ const BarChart = ({ chart }: Props): JSX.Element => {
     );
   }
 
-  const [backgroundColor, borderColor] = getColors(keys.length);
-
   return (
     <Bar
       data={{
@@ -34,8 +31,22 @@ const BarChart = ({ chart }: Props): JSX.Element => {
           {
             label: "Результаты",
             data: Object.values(chart),
-            backgroundColor,
-            borderColor,
+            backgroundColor: [
+              "rgba(255, 99, 132, 0.2)",
+              "rgba(54, 162, 235, 0.2)",
+              "rgba(255, 206, 86, 0.2)",
+              "rgba(75, 192, 192, 0.2)",
+              "rgba(153, 102, 255, 0.2)",
+              "rgba(255, 159, 64, 0.2)",
+            ],
+            borderColor: [
+              "rgba(255, 99, 132, 1)",
+              "rgba(54, 162, 235, 1)",
+              "rgba(255, 206, 86, 1)",
+              "rgba(75, 192, 192, 1)",
+              "rgba(153, 102, 255, 1)",
+              "rgba(255, 159, 64, 1)",
+            ],
             borderWidth: 0.5,
             barPercentage: 0.5,
           },
