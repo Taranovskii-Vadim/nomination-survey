24ad5ad98e85b64f4eef1815901af723e1b348c3
vgTaranovsky <taranovscki.vadim@yandex.ru>
2022-07-11 20:56:25 +0400

    fix question router

---
+++ back/models/Question/constants.ts
@@ -0,0 +1,6 @@
+import { Question } from "./types";
+
+export const QUESTIONS: Question[] = [
+  { id: "1", description: "asdasd" },
+  { id: "2", description: "asdasd" },
+];
--- back/routers/questionRouter.ts
+++ back/routers/questionRouter.ts
@@ -1,6 +1,6 @@
 import { Router, Response } from "express";
+import { QUESTIONS } from "../models/Question/constants";
 
-import Question from "../models/Question";
 import { Question as QuestionType } from "../models/Question/types";
 import { AppRequest } from "../types";
 
@@ -14,7 +14,9 @@ router.get("/:id", async ({ params, user }: AppRequest, res: Response) => {
       throw new Error("Question id is required");
     }
 
-    const question: QuestionType | null = await Question.findById(id);
+    const question: QuestionType | null = QUESTIONS.find(
+      (item) => item.id === id
+    );
 
     if (!question) {
       res.status(404).json({ message: "Question not found" });
