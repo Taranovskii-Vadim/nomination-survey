e234395d3c950f811812ffbc28c218b77404a089
vgTaranovsky <taranovscki.vadim@yandex.ru>
2022-10-28 16:38:49 +0400

    format helpers

--- front/src/pages/helpers.ts
+++ front/src/pages/helpers.ts
@@ -1,10 +1,12 @@
-import { SurveyStatus } from "src/store/types";
-import { UserRole } from "src/store/user/types";
+import { SurveyStatus } from 'src/store/types';
+import { UserRole } from 'src/store/user/types';
 
 export const isHaveAccess = (role: UserRole, status: SurveyStatus): boolean => {
-  if (role === "admin") return true;
-  if (role === "chief" && status === "chiefVote") return true;
-  if (role === "user" && status === "userVote") return true;
+  if (role === 'admin') return true;
+
+  if (role === 'chief' && status === 'chiefVote') return true;
+
+  if (role === 'user' && status === 'userVote') return true;
 
   return false;
 };
