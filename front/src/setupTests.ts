7afe49736b1c0fc77e7f1a9e741443f98ff3521a
vgTaranovsky <taranovscki.vadim@yandex.ru>
2022-11-08 16:13:44 +0400

    remove todo

--- front/src/setupTests.ts
+++ front/src/setupTests.ts
@@ -1,18 +1,8 @@
 import '@testing-library/jest-dom/extend-expect';
 
-// TODO think how to mock axios correctly
 jest.mock('axios', () => ({
   create: () => ({
-    request: () => {
-      const result = new Object([
-        { id: 1, title: 'first', status: 'ready' },
-        { id: 2, title: 'second', status: 'userVote' },
-      ]);
-
-      result.survey = {};
-
-      return Promise.resolve({ data: { result } });
-    },
+    request: () => Promise.resolve({ data: { result: [null] } }),
     interceptors: {
       response: {
         use: jest.fn,
