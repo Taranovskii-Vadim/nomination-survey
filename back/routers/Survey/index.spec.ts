a130d9167b427646d83c3c9ad8f0d33217871881
vgTaranovsky <taranovscki.vadim@yandex.ru>
2022-11-14 22:41:48 +0400

    add survey tests

--- back/routers/Survey/index.spec.ts
+++ back/routers/Survey/index.spec.ts
@@ -2,11 +2,43 @@ import request from 'supertest';
 
 import { server } from '../..';
 
-describe('Survey router', () => {
-  test('get all surveys', async () => {
-    // TODO got no idea how to pass jwt token
-    const response = await request(server).get('/api/surveys');
+let token;
+const BASE = '/api/surveys';
 
-    expect(response.statusCode).toBe(401);
+const get = async (postfix?: string) => {
+  return await request(server)
+    .get(`${BASE}${postfix}`)
+    .set('Cookie', [`token=${token}`]);
+};
+
+describe('Survey router', () => {
+  beforeAll(async () => {
+    const auth = await request(server).post('/api/auth/admin');
+    const cookie = auth.headers['set-cookie'][0];
+    token = cookie.split('token=')[1];
+  });
+
+  test('get all surveys', async () => {
+    const response = await get();
+
+    expect(response.statusCode).toBe(200);
+  });
+
+  test('get survey by correct id', async () => {
+    const response = await get('/1');
+
+    expect(response.statusCode).toBe(200);
+  });
+
+  test('get survey by wrong id', async () => {
+    const response = await get('/1000');
+
+    expect(response.statusCode).toBe(404);
+  });
+
+  test('get survey by string id', async () => {
+    const response = await get('/value');
+
+    expect(response.statusCode).toBe(400);
   });
 });
