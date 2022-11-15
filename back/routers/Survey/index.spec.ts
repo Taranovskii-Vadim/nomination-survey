b10efc82cf110423d909ab49c7e2f2ed0600b041
vgTaranovsky <taranovscki.vadim@yandex.ru>
2022-11-15 20:02:55 +0400

    finish survey tests

--- back/routers/Survey/index.spec.ts
+++ back/routers/Survey/index.spec.ts
@@ -2,12 +2,29 @@ import request from 'supertest';
 
 import { server } from '../..';
 
+import { SurveyStatus } from './types';
+
 let token;
 const BASE = '/api/surveys';
 
+// TODO maybe can create common utils for unit tests
 const get = async (postfix?: string) => {
   return await request(server)
-    .get(`${BASE}${postfix}`)
+    .get(`${BASE}${postfix || ''}`)
+    .set('Cookie', [`token=${token}`]);
+};
+
+const post = async (postfix?: string, payload?: any) => {
+  return await request(server)
+    .post(`${BASE}${postfix || ''}`)
+    .send(payload)
+    .set('Cookie', [`token=${token}`]);
+};
+
+const put = async (postfix?: string, payload?: any) => {
+  return await request(server)
+    .put(`${BASE}${postfix || ''}`)
+    .send(payload)
     .set('Cookie', [`token=${token}`]);
 };
 
@@ -40,5 +57,26 @@ describe('Survey router', () => {
     const response = await get('/value');
 
     expect(response.statusCode).toBe(400);
+  });
+
+  test('get survey results', async () => {
+    const response = await get('/results/user/0');
+
+    expect(response.statusCode).toBe(200);
+  });
+
+  test('post survey results', async () => {
+    const payload = { 1: 3, 2: 8, 3: 5, 4: 4, 5: 2, 6: 1 };
+    const response = await post('/1', payload);
+
+    expect(response.statusCode).toBe(200);
+  });
+
+  test('put survey status', async () => {
+    const payload: SurveyStatus = 'chiefVote';
+
+    const response = await put('/1', payload);
+
+    expect(response.statusCode).toBe(200);
   });
 });
