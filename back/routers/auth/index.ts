9ec4c7b39f7b8bf45956330afb2b6ddfc0fde08a
vgTaranovsky <taranovscki.vadim@yandex.ru>
2023-02-08 21:40:16 +0400

    add postgre to users

--- back/routers/auth/index.ts
+++ back/routers/auth/index.ts
@@ -1,30 +1,27 @@
 import jwt from 'jsonwebtoken';
 import { Response, Request, Router } from 'express';
 
-import { User } from '../../types';
-import FileModel from '../../models/FileModel';
-
+import { database } from '../../db';
 import { formatError } from '../helpers';
 
 const router = Router();
 
 // 5 min
-// const maxAge = 300000;
+const maxAge = 300000;
 // 1 min
-const maxAge = 60000;
+// const maxAge = 60000;
 
 router.post('/:login', async ({ params }: Request<{ login: string }>, res: Response) => {
   try {
     const { login } = params;
 
-    const users = await FileModel.getData<User[]>('users');
-    const result = users.find((item) => item.login === login);
+    const { rows } = await database.query('SELECT * FROM users where login=$1', [login]);
 
-    if (!result) {
+    if (!rows.length) {
       return res.status(404).json({ message: 'User not found' });
     }
 
-    const token = jwt.sign(result, process.env.JWT_KEY);
+    const token = jwt.sign(rows[0], process.env.JWT_KEY);
 
     return res.cookie('token', token, { maxAge }).json();
   } catch (e) {
